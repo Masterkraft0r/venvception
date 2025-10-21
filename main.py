2f57ddaf5946d4ad823a7a51a909c579ee5c3901
Benjamin Bugl <benjamin.bugl@gmx.net>
2025-10-21 13:29:48 +0200

    feat: Rewrite and simplify

--- main.py
+++ main.py
@@ -4,246 +4,200 @@ import argparse as ap
 import dataclasses as d
 import os
 import os.path as op
-import re
+import pathlib as p
 import subprocess as sp
 import sys
 import tomllib
 import typing as t
-
-import click
-
-Command = t.Literal["install", "remove", "install_group", "remove_group", "list"]
-
-Dependency = str
-Dependencies = list[str]
-
-
-class Package(t.TypedDict):
-    name: str
-    dependencies: Dependencies
 
 
 class Include(t.TypedDict):
     include: str
 
 
-Group = list[str | Package | Include]
+PackageSpec = str
+Dependencies = set[PackageSpec]
+Package = tuple[PackageSpec, set[PackageSpec]]
+Tool = PackageSpec | Package
+ToolGroup = set[Tool | Include]
+
+TOMLDependencies = list[PackageSpec]
 
 
-def is_dependency(value: t.Any) -> t.TypeGuard[Dependency]:
+class TOMLPackage(t.TypedDict):
+    name: str
+    dependencies: TOMLDependencies
+
+
+TOMLTool = PackageSpec | TOMLPackage
+TOMLToolGroup = list[TOMLTool | Include]
+
+
+class VenvceptionException(RuntimeError):
+    pass
+
+
+def venvception(extras: list[str]):
+    xdg_data_project = p.Path(os.environ.get("UV_PROJECT_ENVIRONMENT", op.join(os.getcwd(), ".venv")), "share")
+    if not xdg_data_project.is_dir():
+        raise VenvceptionException("Please create a local venv before running venvception.")
+
+    tools: set[Tool] = set()
+    groups: dict[str, ToolGroup] = dict()
+
+    toml_path = p.Path("pyproject.toml")
+    if not toml_path.is_file():
+        raise VenvceptionException("")
+
+    with toml_path.open("rb") as f:
+        toml = tomllib.load(f)
+
+    config = _load_config(toml)
+
+    if "tools" in config and not _is_toml_tool_group(config["tools"]):
+        raise VenvceptionException("")
+    elif "tools" in config:
+        tools = _toml_to_group(config["tools"], False)
+
+    if "groups" in config and not _is_toml_tool_groups(config["groups"]):
+        raise VenvceptionException("")
+    elif "groups" in config:
+        for name, group in t.cast(dict[str, ToolGroup], config["groups"]).items():
+            groups[name] = group
+
+    processed_groups: set[str] = set()
+    for group_name in extras:
+        tools, processed_groups = _process_group(group_name, groups, tools, processed_groups=processed_groups)
+
+    for tool in tools:
+        cmd = "uv tool "
+        match tool:
+            case tool if _is_package_spec(tool):
+                cmd += tool
+            case (name, dependencies):
+                cmd += " ".join((f'--with "{dep}"' for dep in dependencies)) + " " + name
+            case _:
+                raise VenvceptionException("Can not happen.")
+        _ = sp.run(
+            cmd,
+            shell=True,
+            env=os.environ | {"XDG_DATA_HOME": str(xdg_data_project)},
+            encoding="utf-8",
+        )
+
+
+def _load_config(toml: dict[str, t.Any]) -> dict[str, t.Any]:
+    if "tool" not in toml:
+        return {}
+
+    if not isinstance(toml["tool"], dict):
+        raise VenvceptionException("")
+
+    if "venvception" not in toml["tool"]:
+        return {}
+
+    if not isinstance(toml["tool"]["venvception"], dict):
+        raise VenvceptionException("")
+
+    return t.cast(dict[str, t.Any], toml["tool"]["venvception"])
+
+
+def _is_package_spec(value: t.Any) -> t.TypeGuard[PackageSpec]:
     # TODO: Add version specifier check
     return isinstance(value, str)
 
 
-def is_dependencies(value: t.Any) -> t.TypeGuard[Dependencies]:
-    return isinstance(value, list) and all(is_dependency(x) for x in t.cast(list[t.Any], value))
+@t.overload
+def _toml_to_group(toml: TOMLToolGroup, includes_allowed: t.Literal[True]) -> ToolGroup: ...
 
 
-def is_package(value: t.Any) -> t.TypeGuard[Package]:
-    return (
-        isinstance(value, dict)
-        and "name" in value
-        and isinstance(value["name"], str)
-        and "dependencies" in value
-        and is_dependencies(value["dependencies"])
-    )
+@t.overload
+def _toml_to_group(toml: TOMLToolGroup, includes_allowed: t.Literal[False]) -> set[Tool]: ...
 
 
-def is_include(value: t.Any) -> t.TypeGuard[Include]:
+def _toml_to_group(toml: TOMLToolGroup, includes_allowed: bool) -> ToolGroup | set[Tool]:
+    group = ToolGroup()
+    for entry in toml:
+        if _is_include(entry):
+            if includes_allowed:
+                group.add(entry)
+            else:
+                raise VenvceptionException("")
+        elif _is_toml_package(entry):
+            group.add((entry["name"], set(entry["dependencies"])))
+        else:
+            group.add(t.cast(PackageSpec, entry))
+    return group
+
+
+def _is_include(value: t.Any) -> t.TypeGuard[Include]:
     return isinstance(value, dict) and "include" in value and isinstance(value["include"], str)
 
 
+def _is_toml_dependencies(value: t.Any) -> t.TypeGuard[Dependencies]:
+    return isinstance(value, set) and all(_is_package_spec(x) for x in t.cast(set[t.Any], value))
 
-@t.final
-class Venvception:
-    def __init__(self, xdg_data_project: str, groups: dict[str, Group]):
-        self.xdg_data_project = xdg_data_project
-        self.groups = groups
-        self.tools: set[tuple[str, tuple[str, ...]]] = set()
-        stdout = self._uv("tool list", True, False)
-        self.installed_tools = [
-            tool.split(" ")[0].strip() for tool in stdout.split("\n") if tool != "" and not tool.startswith("-")
-        ]
-        self.installed_groups: set[str] = set()
-        self.tools_dir = self._uv("tool dir", True, False)
 
-    def install(self, tool: str, dependencies: tuple[str, ...]):
-        if tool in self.installed_tools:
-            print(f"Tool {tool} already installed.", file=sys.stderr)
-            return
+def _is_toml_package(value: t.Any) -> t.TypeGuard[TOMLPackage]:
+    return (
+        isinstance(value, dict)
+        and "name" in value
+        and _is_package_spec(value["name"])
+        and "dependencies" in value
+        and _is_toml_dependencies(value["dependencies"])
+    )
 
-        self._uv(f"tool install {' '.join((f'--with "{dep}"' for dep in dependencies))} {tool}", False, False)
 
-        if "pyelftools<=0.25" in dependencies:
-            container_path = op.join(
-                self.tools_dir,
-                tool,
-                "lib",
-                f"python{sys.version_info.major}.{sys.version_info.minor}",
-                "site-packages",
-                "elftools",
-                "construct",
-                "lib",
-                "container.py",
-            )
-            _ = sp.run(
-                f"sed -i '5s/from collections import MutableMapping/from collections\\.abc import MutableMapping/' '{container_path}'",
-                shell=True,
-                stdout=sp.PIPE,
-                stderr=sp.PIPE,
-            )
-            print(container_path)
+def _is_toml_tool_group(value: t.Any) -> t.TypeGuard[TOMLToolGroup]:
+    return isinstance(value, list) and all(
+        _is_package_spec(entry) or _is_toml_package(entry) or _is_include(entry) for entry in t.cast(list[t.Any], value)
+    )
 
-    def remove(self, tool: str):
-        if tool not in self.installed_tools:
-            print(f"Tool {tool} not installed.", file=sys.stderr)
-            return
 
-        self._uv(f"tool uninstall {tool}", False, False)
+def _is_toml_tool_groups(value: t.Any) -> t.TypeGuard[dict[str, TOMLToolGroup]]:
+    return isinstance(value, dict) and all(
+        isinstance(name, str) and _is_toml_tool_group(group) for name, group in t.cast(dict[t.Any, t.Any], value)
+    )
 
-    def install_group(self, groups: list[str]):
-        for group in set(groups):
-            self._accumulate_tools(group)
 
-        for tool, dependencies in self.tools:
-            self.install(tool, dependencies)
+def _process_group(
+    group_name: str,
+    groups: dict[str, ToolGroup],
+    tools: set[Tool],
+    processed_groups: set[str] | None = None,
+) -> tuple[set[Tool], set[str]]:
+    if group_name not in groups:
+        raise VenvceptionException("")
+    if processed_groups is not None and group_name in processed_groups:
+        print(f"Group {group_name} already processed. Skipping.", file=sys.stderr)
+        return (tools, processed_groups)
 
-    def remove_group(self, groups: list[str]):
-        for group in set(groups):
-            self._accumulate_tools(group)
+    if processed_groups is None:
+        processed_groups = set()
+    processed_groups.add(group_name)
 
-        for tool, _ in self.tools:
-            self.remove(tool)
+    group = groups[group_name]
+    for tool in group:
+        if _is_include(tool):
+            if tool["include"] not in groups:
+                raise VenvceptionException("")
+            tools, processed_groups = _process_group(tool["include"], groups, tools, processed_groups)
+        else:
+            tools.add(t.cast(Tool, tool))
 
-    def list(self, _: list[str]):
-        if self.installed_tools:
-            print("\n".join(self.installed_tools))
+    return tools, processed_groups
 
-    def _accumulate_tools(self, group: str):
-        if group not in self.groups:
-            print(f"Group {group} not found.", file=sys.stderr)
-            return
-        elif group in self.installed_groups:
-            print(f"Group {group} already installed.", file=sys.stderr)
-            return
 
-        self.installed_groups.add(group)
+def cli(argv: list[str]):
+    parser = ap.ArgumentParser("venvception")
+    parser.add_argument("--extra", action="append", default=[])
+    args = parser.parse_args(argv)
 
-        for entry in self.groups[group]:
-            if is_include(entry):
-                self._accumulate_tools(entry["include"])
-            elif is_package(entry):
-                self.tools.add((entry["name"], tuple(entry["dependencies"])))
-            elif is_dependency(entry):
-                self.tools.add((entry, ()))
-            else:
-                print(f"Group entry {entry} is not valid. Skipping.", file=sys.stderr)
-
-    @t.overload
-    def _uv(self, cmd: str, capture_stdout: t.Literal[False], capture_stderr: t.Literal[False]) -> None: ...
-
-    @t.overload
-    def _uv(self, cmd: str, capture_stdout: t.Literal[True], capture_stderr: t.Literal[False]) -> str: ...
-
-    @t.overload
-    def _uv(self, cmd: str, capture_stdout: t.Literal[False], capture_stderr: t.Literal[True]) -> str: ...
-
-    @t.overload
-    def _uv(self, cmd: str, capture_stdout: t.Literal[True], capture_stderr: t.Literal[True]) -> tuple[str, str]: ...
-
-    def _uv(self, cmd: str, capture_stdout: bool = False, capture_stderr: bool = False) -> str | tuple[str, str] | None:
-        proc = sp.run(
-            "uv " + cmd,
-            shell=True,
-            stdout=sp.PIPE if capture_stdout else None,
-            stderr=sp.PIPE if capture_stderr else None,
-            env=os.environ | {"XDG_DATA_HOME": self.xdg_data_project},
-            encoding="utf-8",
-        )
-
-        if capture_stdout and capture_stderr:
-            return (proc.stdout, proc.stderr)
-        elif capture_stdout:
-            return proc.stdout
-        elif capture_stderr:
-            return proc.stderr
+    try:
+        venvception(args.extra)
+    except VenvceptionException as e:
+        print(f"ERROR: {str(e)}", file=sys.stderr)
 
 
 if __name__ == "__main__":
-    xdg_data_project = op.join(os.environ.get("UV_PROJECT_ENVIRONMENT", op.join(os.getcwd(), ".venv")), "share")
-    if not op.isdir(xdg_data_project):
-        print("Please create a local venv before running venvception.", file=sys.stderr)
-        sys.exit(1)
-
-    try:
-        with open("pyproject.toml", "rb") as f:
-            pyproject = tomllib.load(f)
-    except:
-        print("pyproject.toml could not be read.", file=sys.stderr)
-        sys.exit(1)
-
-    class VenvceptionConfig(t.TypedDict):
-        tools: Group
-        groups: dict[str, Group]
-
-    tool_section = t.cast(dict[str, dict[str, VenvceptionConfig]] | None, pyproject.get("tool", None))
-    if tool_section is None:
-        # No tools to be installed; not an error
-        sys.exit(0)
-
-    config = t.cast(VenvceptionConfig | None, tool_section.get("venvception", None))
-    if config is None:
-        # No tools to be installed; not an error
-        sys.exit(0)
-
-    groups = config.get("groups", {})
-    groups["default"] = config.get("tools", [])
-
-    venvception = Venvception(xdg_data_project, groups)
-
-    package_parser = ap.ArgumentParser(add_help=False)
-    _ = package_parser.add_argument("package")
-
-    parser = ap.ArgumentParser("venvception")
-    subcmds = parser.add_subparsers(dest="subcommand", metavar="SUBCOMMAND")
-
-    install_parser = subcmds.add_parser("install")
-    _ = install_parser.add_argument("tool", metavar="TOOL")
-    _ = install_parser.add_argument("dependencies", metavar="DEPENDENCIES", nargs="*")
-
-    remove_parser = subcmds.add_parser("remove")
-    _ = remove_parser.add_argument("tool", metavar="TOOL")
-
-    install_group_parser = subcmds.add_parser("install_group")
-    _ = install_group_parser.add_argument("group", metavar="GROUP", nargs="*", default=["default"])
-
-    remove_group_parser = subcmds.add_parser("remove_group")
-    _ = remove_group_parser.add_argument("group", metavar="GROUP", nargs="*", default=["default"])
-
-    list_parser = subcmds.add_parser("list")
-
-    @d.dataclass
-    class Arguments:
-        subcommand: Command = "install"
-        group: list[str] = d.field(default_factory=list)
-        tool: str = ""
-        dependencies: list[str] = d.field(default_factory=list)
-
-    args = Arguments()
-    _ = parser.parse_args(sys.argv[1:], args)
-
-    match args.subcommand:
-        case "install":
-            venvception.install(args.tool, tuple(args.dependencies))
-
-        case "remove":
-            venvception.remove(args.tool)
-
-        case "install_group":
-            venvception.install_group(args.group)
-
-        case "remove_group":
-            venvception.remove_group(args.group)
-
-        case "list":
-            venvception.list(args.group)
+    sys.exit(cli(sys.argv[1:]))
